9f6e10644daae37c00c58763f8a5f648c075c895
Yan <crowbeat.47@gmail.com>
2025-07-24 17:14:25 +0500

    style: добавлен эффект при навидении и transition для всех икнок и кнопок

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,35 +4,47 @@ export default function Footer() {
   return (
     <footer className="">
       <div className="container">
-        <div className="flex flex-wrap sm:justify-between justify-evenly items-center py-6 gap-5">
+        <div className="flex flex-wrap font-semibold sm:justify-between justify-evenly items-center py-6 gap-5">
           <a href="#">
             <img src={logo} alt="Logo" />
           </a>
           <nav className="flex flex-wrap items-start justify-center gap-13">
             <ul className="flex flex-col items-center space-y-1">
               <li>
-                <a href="#">About us</a>
+                <a href="#" className="hover:text-gray-600">
+                  About us
+                </a>
               </li>
               <li>
-                <a href="#">Contacts</a>
+                <a href="#" className="hover:text-gray-600">
+                  Contacts
+                </a>
               </li>
               <li>
-                <a href="#">Build PC</a>
+                <a href="#" className="hover:text-gray-600">
+                  Build PC
+                </a>
               </li>
             </ul>
             <ul className="flex flex-col items-start space-y-1">
               <li>
-                <a href="#">Reviews</a>
+                <a href="#" className="hover:text-gray-600">
+                  Reviews
+                </a>
               </li>
               <li>
-                <a href="#">Repair</a>
+                <a href="#" className="hover:text-gray-600">
+                  Repair
+                </a>
               </li>
               <li>
-                <a href="#">For wholesale buyers</a>
+                <a href="#" className="hover:text-gray-600">
+                  For wholesale buyers
+                </a>
               </li>
             </ul>
           </nav>
-          <a href="#" className="text-[#019EE2]">
+          <a href="#" className="text-[#019EE2] hover:text-[#0179B4]">
             Privacy Policy
           </a>
         </div>
