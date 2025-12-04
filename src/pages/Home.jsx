088e608ed4f3b7372c9357b999f1ef6a7d05700d
Mamagul <mamagul.gapurova@mail.ru>
2025-12-04 15:15:36 +0500

    DeletePages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Catalog from "../components/Catalog";
+// import Catalog from "../components/Catalog";
 import Header from "../components/Header";
 import FirstScreen from "../components/FirstScreen";
 import About from "../components/About";
-import Services from "../components/Services";
-import TechServices from "../components/TechServices";
+// import Services from "../components/Services";
+// import TechServices from "../components/TechServices";
 import AssemblyToOrder from "../components/AssemblyToOrder";
 import Footer from "../components/Footer";
-import Upgrade from "../components/Upgrade";
+// import Upgrade from "../components/Upgrade";
 import Contacts from "../components/Contacts";
 import OfficeMaintenance from "../components/OfficeMaintenance";
 import B2b from "../components/B2b";
@@ -21,11 +21,11 @@ export default function Home() {
       <div className="main">
         <FirstScreen />
         <About />
-        <Catalog />
-        <Services />
+        {/* <Catalog /> */}
+        {/* <Services /> */}
         <AssemblyToOrder />
-        <TechServices />
-        <Upgrade />
+        {/* <TechServices /> */}
+        {/* <Upgrade /> */}
         <OfficeMaintenance />
         <B2b />
         <Reviews />
